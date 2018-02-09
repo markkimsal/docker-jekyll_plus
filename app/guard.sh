0fc30e1560177a9039b51f1232639bfde0ad411d
Mark Kimsal <metrofindings@gmail.com>
2018-02-09 15:57:18 -0500

    Make guard installation location independant

--- app/guard.sh
+++ app/guard.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /app/
-HOME=/src/ exec ruby /var/lib/gems/2.2.0/gems/guard-2.13.0/bin/_guard-core -i -w /src/_site/
+HOME=/src/ exec ruby /usr/local/bin/_guard-core -i -w /src/_site/
